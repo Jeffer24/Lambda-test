e14a3d2de99b4db853fabfbe069825fc3ff70703
Jefferr24 <jefferr24@gmail.com>
2023-03-30 22:16:55 -0500

    feat: Add propertie to Object Perfiles

--- application/helpers/perfil-dto.js
+++ application/helpers/perfil-dto.js
@@ -1,34 +1,31 @@
 const getPerfilFromDBDto = ({
-    idPerfiles,
-    nombrePerfil,
-    estado
+  idPerfiles,
+  nombrePerfil,
+  tipoCobertura,
+  estado,
 }) => ({
-    idPerfiles: idPerfiles,
-    nombrePerfil: nombrePerfil,
-    estado: estadoChange(estado)
+  idPerfiles: idPerfiles,
+  tipoCobertura: tipoCobertura,
+  nombrePerfil: nombrePerfil,
+  estado: estadoChange(estado),
 });
 
 const estadoChange = (estado) => {
-    const uint32array = new Uint32Array(estado);
-    return uint32array[0];
+  const uint32array = new Uint32Array(estado);
+  return uint32array[0];
 };
 
-const getPerfilesFromDBArray = (perfilesDB) => perfilesDB.map(getPerfilFromDBDto);
+const getPerfilesFromDBArray = (perfilesDB) =>
+  perfilesDB.map(getPerfilFromDBDto);
 
-const getDBFromPerfilDto = ({
-    idPerfiles,
-    nombrePerfil,
-    estado
-}) => ({
-    idPerfiles: idPerfiles,
-    nombrePerfil: nombrePerfil,
-    estado: estado
+const getDBFromPerfilDto = ({ idPerfiles, nombrePerfil, estado }) => ({
+  idPerfiles: idPerfiles,
+  nombrePerfil: nombrePerfil,
+  estado: estado,
 });
 
 module.exports = {
-    getPerfilFromDBDto,
-    getPerfilesFromDBArray,
-    getDBFromPerfilDto,
-}
-
- 
+  getPerfilFromDBDto,
+  getPerfilesFromDBArray,
+  getDBFromPerfilDto,
+};
