2952705d864196d1b5079fdaeb0df5c73e884123
Jefferr24 <jefferr24@gmail.com>
2023-03-30 22:39:25 -0500

    feat: Add propertie to Object Perfiles method create and update

--- application/model_adapters/perfil-adapter.js
+++ application/model_adapters/perfil-adapter.js
@@ -1,46 +1,47 @@
-const perfilQuery = require('../../infraestructure/repositories/perfiles-query');
-const perfilDto = require('../helpers/perfil-dto');
+const perfilQuery = require("../../infraestructure/repositories/perfiles-query");
+const perfilDto = require("../helpers/perfil-dto");
 
 const findPerfiles = async () => {
-    const data = await perfilQuery.findPerfiles();
-    return perfilDto.getPerfilesFromDBArray(data);
-}
+  const data = await perfilQuery.findPerfiles();
+  return perfilDto.getPerfilesFromDBArray(data);
+};
 
 const findOnePerfil = async (id) => {
-    const perf = await perfilQuery.findOnePerfil(id);
-    if(!perf) return null;
-    return perfilDto.getPerfilFromDBDto(perf);
-}
+  const perf = await perfilQuery.findOnePerfil(id);
+  if (!perf) return null;
+  return perfilDto.getPerfilFromDBDto(perf);
+};
 
-const createPerfil = async ({
+const createPerfil = async ({ nombrePerfil, estado, tipoCobertura }) => {
+  return await perfilQuery.createPerfil({
     nombrePerfil,
-    estado
-}) => {
-    return await perfilQuery.createPerfil(
-        {
-            nombrePerfil,
-            estado
-        }
-    );
-}
+    estado,
+    tipoCobertura,
+  });
+};
 
-const updatePerfil = async ({
-    nombrePerfil, estado
-}, idPerfiles) => {
-    return await perfilQuery.updatePerfil({
-        nombrePerfil, 
-        estado
-    }, idPerfiles)
-}
+const updatePerfil = async (
+  { nombrePerfil, estado, tipoCobertura },
+  idPerfiles
+) => {
+  return await perfilQuery.updatePerfil(
+    {
+      nombrePerfil,
+      estado,
+      tipoCobertura,
+    },
+    idPerfiles
+  );
+};
 
 const deletePerfil = async (idPerfiles) => {
-    return await perfilQuery.deletePerfil(idPerfiles);
-}
+  return await perfilQuery.deletePerfil(idPerfiles);
+};
 
 module.exports = {
-    findPerfiles,
-    findOnePerfil,
-    createPerfil,
-    updatePerfil,
-    deletePerfil,
-}
+  findPerfiles,
+  findOnePerfil,
+  createPerfil,
+  updatePerfil,
+  deletePerfil,
+};
