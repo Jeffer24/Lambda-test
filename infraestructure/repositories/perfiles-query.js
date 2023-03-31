d7339c84d502196e7d1997ae2de01dc53f95e8cc
Jefferr24 <jefferr24@gmail.com>
2023-03-30 23:17:08 -0500

    fix: Modify query insert and update perfiles

--- infraestructure/repositories/perfiles-query.js
+++ infraestructure/repositories/perfiles-query.js
@@ -1,48 +1,37 @@
-const { db } = require('./connections');
-const TableName = 'Perfiles';
-
+const { db } = require("./connections");
+const TableName = "Perfiles";
 
 const findPerfiles = async () => {
-    return db.select().table(TableName);
-}
+  return db.select().table(TableName);
+};
 
 const findOnePerfil = async (id) => {
-    //return db.select().table(TableName).innerJoin(TableName, estudios.id, games.id)
-    return db.select().table(TableName).where('idPerfiles', id).first();
-}
-const createPerfil = async ({
+  //return db.select().table(TableName).innerJoin(TableName, estudios.id, games.id)
+  return db.select().table(TableName).where("idPerfiles", id).first();
+};
+const createPerfil = async ({ nombrePerfil, estado, tipoCobertura }) => {
+  return await db(TableName).insert({
     nombrePerfil,
-    estado
-}) => {
-    
-    return await db(TableName).insert({
-        nombrePerfil,
-        estado
-    });
-}
-const updatePerfil = async ({
-    nombrePerfil, 
-    estado
-}, id) => {
-    return await db(TableName)
-    .where('idPerfiles', id)
-    .update({
-        nombrePerfil: nombrePerfil,
-        estado: estado
-    });
-}
-
+    estado,
+    tipoCobertura,
+  });
+};
+const updatePerfil = async ({ nombrePerfil, estado, tipoCobertura }, id) => {
+  return await db(TableName).where("idPerfiles", id).update({
+    nombrePerfil: nombrePerfil,
+    estado: estado,
+    tipoCobertura: tipoCobertura,
+  });
+};
 
 const deletePerfil = async (id) => {
-    return await db(TableName)
-    .where('idPerfiles', id)
-    .del();
-}
+  return await db(TableName).where("idPerfiles", id).del();
+};
 
 module.exports = {
-    findPerfiles,
-    findOnePerfil,
-    createPerfil,
-    updatePerfil,
-    deletePerfil,
-}
+  findPerfiles,
+  findOnePerfil,
+  createPerfil,
+  updatePerfil,
+  deletePerfil,
+};
